2734bac0bfbdb853dbb49f1f97ace3242dac6c0c
Jp Orihuela <112457674+OrihuelaAraiza@users.noreply.github.com>
2025-11-11 17:20:04 -0600

    Initial Commit

---
+++ Visuaid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Visuaid
+//
+//  Created by Juan Pablo Orihuela Araiza on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Visuaid/VisuaidApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisuaidApp.swift
+//  Visuaid
+//
+//  Created by Juan Pablo Orihuela Araiza on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VisuaidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
